bf371365dc6ffb7c0dbaa2870a8698b14f83e1ff
Radoslav Filipov <radoslav.filipov@sap.com>
2020-05-27 19:48:05 +0300

    Add main entry point using maven assembly plugin

---
+++ src/main/java/com/secrets/vault/SecretsVaultMain.java
@@ -0,0 +1,15 @@
+/**
+ * Created on May 27, 2020 by Radoslav Filipov
+ */
+package com.secrets.vault;
+
+/**
+ * @author Filipov, Radoslav
+ */
+public class SecretsVaultMain {
+
+  public static void main(String[] args) {
+    // TODO: Handle commands and arguments properly
+  }
+
+}
